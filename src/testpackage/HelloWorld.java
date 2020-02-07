f33c7c03351d6dfc9934ff0e49ec5884be6fe75a
Simone <nike10@live.it>
2020-02-07 23:23:46 +0100

    1

---
+++ src/testpackage/HelloWorld.java
@@ -0,0 +1,18 @@
+
+
+/*
+ * Copyright 2020 Simone Festa
+ *
+ * This file is a part of weSpend project developed for the course
+ * ISPW (A.Y. 2019-2020) at Università di Tor Vergata in Rome
+ */
+
+package testpackage;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
