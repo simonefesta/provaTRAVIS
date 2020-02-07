a5e25c9f0e65d1a140bd3bd5678107eaf5d90f72
Simone Festa <55951548+simonefesta@users.noreply.github.com>
2020-02-08 00:37:41 +0100

    Update HelloWorld.java

--- src/testpackage/HelloWorld.java
+++ src/testpackage/HelloWorld.java
@@ -13,6 +13,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		 Logger logger = Logger.getLogger(HelloWorld.class.getName());
 		 logger.info("Hello World");
+		 System.out.println("ciao bella");
 	}
 
 }
