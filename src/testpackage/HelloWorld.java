7adf3c7e416662291162c7f41df65ea7df566a2a
Simone <nike10@live.it>
2020-02-08 00:05:12 +0100

    1

--- src/testpackage/HelloWorld.java
+++ src/testpackage/HelloWorld.java
@@ -8,11 +8,11 @@
  */
 
 package testpackage;
-
+import java.util.logging.Logger;
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-
+		 Logger logger = Logger.getLogger(HelloWorld.class.getName());
+		 logger.info("Hello World");
 	}
 
 }
